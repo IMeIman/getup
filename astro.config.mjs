a0f14ca3270cda444c04edec7f5f1336c068a9c2
imeiman <melfer.creativos@gmail.com>
2026-08-27 20:07:46 -0700

    new-update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://IMeIman.github.io",
+  base: "/getup/",
   output: "static",
   vite: {
     plugins: [tailwindcss()],
